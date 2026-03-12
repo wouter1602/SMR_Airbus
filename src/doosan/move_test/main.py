3fda4b859e7ec3549cab2ebb350c9a840af9fef4
Wouter <3747759+wouter1602@users.noreply.github.com>
2026-03-12 12:04:39 +0100

    Added all the struct functions

--- src/doosan/move_test/main.py
+++ src/doosan/move_test/main.py
@@ -29,19 +29,19 @@ def main():
 
         time.sleep(5)
 
-        status = robot.set_digital_output(doosan_drfl.GPIO_CTRLBOX_DIGITAL_INDEX.Gpio_ctrlbox_digital_index_1, True)
+        status = robot.set_digital_output(doosan_drfl.GPIO_CTRLBOX_DIGITAL_INDEX.Index_1, True)
 
         print(f"Io status: {status}")
 
-        status_1 = robot.get_digital_output(doosan_drfl.GPIO_CTRLBOX_DIGITAL_INDEX.Gpio_ctrlbox_digital_index_1)
+        status_1 = robot.get_digital_output(doosan_drfl.GPIO_CTRLBOX_DIGITAL_INDEX.Index_1)
 
         print(f"Current status of output 1 is: {status_1}")
 
         time.sleep(10)
 
-        status = robot.set_digital_output(doosan_drfl.GPIO_CTRLBOX_DIGITAL_INDEX.Gpio_ctrlbox_digital_index_1, False)
+        status = robot.set_digital_output(doosan_drfl.GPIO_CTRLBOX_DIGITAL_INDEX.Index_1, False)
 
-        status_1 = robot.get_digital_output(doosan_drfl.GPIO_CTRLBOX_DIGITAL_INDEX.Gpio_ctrlbox_digital_index_1)
+        status_1 = robot.get_digital_output(doosan_drfl.GPIO_CTRLBOX_DIGITAL_INDEX.Index_1)
 
         print(f"Current status of output 1 is: {status_1}")
 
